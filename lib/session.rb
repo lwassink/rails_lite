232542bf8deab25dc2bc104f633f0e6862c4a8ca
Luke Wassink <lwassink@gmail.com>
2016-10-11 15:23:17 -0700

    finish part I

--- lib/session.rb
+++ lib/session.rb
@@ -4,16 +4,23 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    rla = req.cookies['_rails_lite_app']
+    @cookie = JSON.parse(rla) unless rla.nil?
+    @cookie ||= {}
   end
 
   def [](key)
+    @cookie[key]
   end
 
   def []=(key, val)
+    @cookie[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    attributes = {path: '/', value: @cookie.to_json}
+    res.set_cookie('_rails_lite_app', attributes)
   end
 end
