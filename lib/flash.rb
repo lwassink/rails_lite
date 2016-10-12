cd5ca986eeb66040fa4ab10091f1d6b5d2217127
Luke Wassink <lwassink@gmail.com>
2016-10-11 17:57:55 -0700

    add usefull errors

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,4 +1,29 @@
 require 'json'
 
 class Flash
+  def initialize(req)
+    @req = req
+    rs = req.cookies['_rails_lite_app_flash']
+    @returned_store = rs.nil? ? {} : JSON.parse(rs)
+    @store_to_send = {}
+    @now_store = {}
+  end
+
+  def [](key)
+    total_store = @returned_store.merge(@store_to_send).merge(@now_store)
+    total_store[key.to_s]
+  end
+
+  def []=(key, value)
+    @store_to_send[key] = value
+  end
+
+  def store_flash(res)
+    attributes = {path: '/', value: @store_to_send.to_json}
+    res.set_cookie('_rails_lite_app_flash', attributes)
+  end
+
+  def now
+    @now_store
+  end
 end
--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,41 @@
 require 'erb'
 
 class ShowExceptions
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      @app.call(env)
+    rescue Exception => e
+      p "I happend, ############################"
+      return ['500', {'Content-type' => 'text/html'}, render_exception(e)]
+    end
   end
 
   private
 
   def render_exception(e)
-  end
+    message = <<-HTML
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <title>Error #{e.class.name}</title>
+      </head>
+      <body>
+      <h1>#{e.class.name}</h1>
+      Message: #{e.message}
+      <br>
+      <br>
+      Backtrace: #{e.backtrace}
+      </body>
+      </html>
+    HTML
 
+    message
+  end
 end
+
